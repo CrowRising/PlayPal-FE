e80b3f41a0a4e1e8c984e897e41b0bded9eec949
Crow Rising <miscbyrd@gmail.com>
2023-06-02 14:37:25 -0600

    chore: add route for playgrounds

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'users#show'
+
+  get '/playgrounds', to: 'playgrounds#index'
 end
