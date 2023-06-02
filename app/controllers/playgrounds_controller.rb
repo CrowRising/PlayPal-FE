581e34bad4b1c33244f796792b0fc9ffc947da16
Crow Rising <miscbyrd@gmail.com>
2023-06-02 14:38:19 -0600

    wip:

---
+++ app/controllers/playgrounds_controller.rb
@@ -0,0 +1,11 @@
+class PlaygroundsController < ApplicationController
+
+  def index
+    if params[:location].present?
+      # facade will go here
+    else
+      redirect_to root_path
+      flash[:error] = "Error: Must add location!"
+    end
+  end
+end
