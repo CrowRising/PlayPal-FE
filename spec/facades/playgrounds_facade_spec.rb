89ac7faa0d70d943eeed057f02b88ca082a1a70f
Thomas Hawley <thawley22@turing.edu>
2023-06-02 16:13:25 -0600

    test: Add initial tests for playgrounds facade

---
+++ spec/facades/playgrounds_facade_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe PlaygroundsFacade do
+  before(:each) do
+    stub_request(:get, "http://localhost:3000/api/v0/playgrounds/90210/1").
+    with(
+      headers: {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v2.7.5'
+      }).
+    to_return(status: 200, body: JSON.generate("data": [
+      {
+        "type": "playground",
+        "id": "23",
+        "attributes": {
+        "playground_name": "Fehringer",
+        "playground_address": "1400 U Street ",
+        "average_rating": "2.5"
+        }
+      },
+      {
+        "type": "playground",
+        "id": "23",
+        "attributes": {
+        "playground_name": "Birds Nest",
+        "playground_address": "1700 U Street",
+        "average_rating": "2.7"
+        }
+      },
+        ]), headers: {})
+  end
+
+  describe 'Playgrounds Facade' do
+    it 'exists' do
+      playground = PlaygroundsFacade.new('80123', 1)
+      
+      expect(playground).to be_a PlaygroundsFacade
+    end
+  end
+
+  describe 'Can create playground objects' do
+    it '#playgrounds' do
+      playground = PlaygroundsFacade.new('80123', 1)
+      
+      expect(playground.playgrounds).to be_an Array
+      expect(playground.playgrounds.first).to be_a Playground
+    end
+  end
+end
