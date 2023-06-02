d789802d46617f4d9bc41edab3c492f5b38543c5
Crow Rising <miscbyrd@gmail.com>
2023-06-01 19:16:30 -0600

    chore: add dashboard path

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'auth/:provider/callback', to: 'sessions#create'
+  get '/dashboard', to: 'dashboard#show'
 end
