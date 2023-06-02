018f72121f5db0cbb22c34d3b907ae4b04372032
Thomas Hawley <thawley22@turing.edu>
2023-06-02 16:13:49 -0600

    feat: Add initial methods to playgrounds facade

--- app/facades/playgrounds_facade.rb
+++ app/facades/playgrounds_facade.rb
@@ -1,4 +1,22 @@
-# frozen_string_literal: true
-
 class PlaygroundsFacade
+
+  def initialize(location, radius)
+    @location = location
+    @radius = radius
+  end
+
+  def playgrounds
+    @_playgrounds ||= playgrounds_data.map do |pg|
+      Playground.new(pg)
+    end
+  end
+
+  private
+  def service
+    @_service ||= PlaygroundsService.new
+  end
+
+  def playgrounds_data
+    @_playgrounds_data ||= service.get_playgrounds(@location, @radius)[:data]
+  end
 end
