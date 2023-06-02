0c0291220a30728e0418a73921182eca62590d37
Crow Rising <miscbyrd@gmail.com>
2023-06-01 19:15:24 -0600

    feat: add to controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
 
   def create
-    require 'pry'; binding.pry
+    auth_hash = request.env['omniauth.auth']
+    google_id = auth_hash['uid']
+    name = auth_hash['info']['name']
+    email = auth_hash['info']['email']
+    token = auth_hash['credentials']['token']
+
+    user = User.find_or_create_by(email: email)
+    user.update(google_id: google_id, token: token)
+
+    session[:user_id] = user.index
+
+    redirect_to '/dashboard'
+
   end
 end
