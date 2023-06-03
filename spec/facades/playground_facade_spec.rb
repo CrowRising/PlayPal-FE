282ed6648675bc42f8289f8e665839ea583137a7
Crow Rising <miscbyrd@gmail.com>
2023-06-03 11:05:04 -0600

    test: add test for review method

--- spec/facades/playground_facade_spec.rb
+++ spec/facades/playground_facade_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PlaygroundFacade do
-  before(:each) do
-    stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/2')
+  describe 'Playground Facade' do
+    it 'exists and can create a single playground object' do
+      stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/2')
       .with(
         headers: {
           'Accept' => '*/*',
@@ -21,23 +22,47 @@ RSpec.describe PlaygroundFacade do
                                                        }
                                                      }
                                                   } ), headers: {})
-  end
-
-  describe 'Playground Facade' do
-    it 'exists' do
       pg = PlaygroundFacade.new('2')
-
+      
       expect(pg).to be_a PlaygroundFacade
-    end
-
-    it 'can create a single playground object' do
-      pg = PlaygroundFacade.new('2')
-
       expect(pg.playground).to be_a Playground
       expect(pg.playground_id).to eq('2')
       expect(pg.playground_name).to eq('Fehringer')
       expect(pg.playground_address).to eq('Full address')
       expect(pg.playground_rating).to eq('4.2')
+    end
+
+    it 'can get reviews for single playground' do
+      stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/24/reviews')
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Faraday v2.7.5'
+        }
+      )
+      .to_return(status: 200, body: JSON.generate({ "data": [
+                                                  {
+                                                    "id": "322458",
+                                                    "type": "review",
+                                                    "attributes": {
+                                                        "comment": "comment",
+                                                        "user_id": "12",
+                                                        "rating": "4.2",
+                                                        "image": "image",
+                                                        "playground_id": "24"
+                                                     }
+                                                   }
+                                                ] }), headers: {})
+                                      
+      pg = PlaygroundFacade.new('24').review
+
+      expect(pg).to be_an Array
+      expect(pg.first.id).to eq('322458')
+      expect(pg.first.comment).to eq('comment')
+      expect(pg.first.rating).to eq('4.2')
+      expect(pg.first.user_id).to eq('12')
+      expect(pg.first.playground_id).to eq('24')
     end
   end
 end
