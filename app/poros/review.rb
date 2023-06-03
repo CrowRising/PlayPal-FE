24d4f4845df42d238b99d3d01a933b165ebb3542
Crow Rising <miscbyrd@gmail.com>
2023-06-03 10:09:32 -0600

    feat: create review poros

---
+++ app/poros/review.rb
@@ -0,0 +1,17 @@
+class Review
+  attr_reader :id,
+              :comment,
+              :user_id,
+              :rating,
+              :image,
+              :playground_id
+              
+  def initialize(data)
+    @id = data[:id]
+    @comment = data[:attributes][:comment]
+    @user_id = data[:attributes][:user_id]
+    @rating = data[:attributes][:rating]
+    @image = data[:attributes][:image]
+    @playground_id = data[:attributes][:playground_id]
+  end
+end
