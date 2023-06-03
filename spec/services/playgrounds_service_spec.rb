4af52d14c9467f53ba8ae1fa3c2b15fa57272a1f
Thomas Hawley <thawley22@turing.edu>
2023-06-02 21:40:28 -0600

    test: Add test for #get_playground method

--- spec/services/playgrounds_service_spec.rb
+++ spec/services/playgrounds_service_spec.rb
@@ -50,6 +50,39 @@ RSpec.describe PlaygroundsService do
       expect(pg[:attributes]).to have_key(:rating)
     end
   end
+
+  it 'establishes connection for a single playground' do
+    stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/2')
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Faraday v2.7.5'
+        }
+      )
+      .to_return(status: 200, body: JSON.generate({ "data": 
+                                                    {
+                                                      "id": "322474",
+                                                      "type": "review",
+                                                      "attributes": {
+                                                          "playground_id": "2",
+                                                          "playground_name": "Fehringer",
+                                                          "playground_address": "Full address",
+                                                          "rating": "4.2"
+                                                       }
+                                                     }
+                                                  } ), headers: {})
+
+    playground = PlaygroundsService.new.get_playground(2)
+    expect(playground).to be_a Hash
+    expect(playground).to have_key(:data)
+    expect(playground[:data]).to have_key(:id)
+    expect(playground[:data]).to have_key(:type)
+    expect(playground[:data][:attributes]).to have_key(:playground_id)
+    expect(playground[:data][:attributes]).to have_key(:playground_name)
+    expect(playground[:data][:attributes]).to have_key(:playground_address)
+    expect(playground[:data][:attributes]).to have_key(:rating)
+  end
 end
 
 # if time permits add testing to check key value returns
