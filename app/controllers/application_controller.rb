cdcdc11c53553994880e385e57ffd6f6d19c696b
Thomas Hawley <thawley22@turing.edu>
2023-06-03 11:59:32 -0600

    refactor: Add current user helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+  def current_user
+    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
