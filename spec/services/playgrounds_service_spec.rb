bf4ffb04cc1ab6244f555a04321d0faa25de8f24
Thomas Hawley <thawley22@turing.edu>
2023-06-03 12:32:12 -0600

    refactor: Move stubbed requests in the playgrounds service spec to a fixture file

--- spec/services/playgrounds_service_spec.rb
+++ spec/services/playgrounds_service_spec.rb
@@ -3,40 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe PlaygroundsService do
-  let(:get_playgrounds) { PlaygroundsService.new.get_playgrounds(90_210, 1600) }
-
-  before(:each) do
-    stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/90210/1600')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent' => 'Faraday v2.7.5'
-        }
-      )
-      .to_return(status: 200, body: JSON.generate("data": [
-                                                    {
-                                                      "type": 'playground',
-                                                      "id": '23',
-                                                      "attributes": {
-                                                        "playground_name": 'Fehringer',
-                                                        "playground_address": '1400 U Street ',
-                                                        "rating": '2.5'
-                                                      }
-                                                    },
-                                                    {
-                                                      "type": 'playground',
-                                                      "id": '23',
-                                                      "attributes": {
-                                                        "playground_name": 'Birds Nest',
-                                                        "playground_address": '1700 U Street',
-                                                        "rating": '2.7'
-                                                      }
-                                                    }
-                                                  ]), headers: {})
-  end
-
   it 'establishes connection for playgrounds in area' do
+    stubbed_response = File.read('spec/fixtures/playbrounds_data.json')
+    stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/90210/1600')
+    .to_return(status: 200, body: stubbed_response)
+
+    get_playgrounds = PlaygroundsService.new.get_playgrounds(90210, 1600)
+
     expect(get_playgrounds).to be_a Hash
     expect(get_playgrounds).to have_key(:data)
     expect(get_playgrounds[:data]).to be_an Array
@@ -52,59 +25,24 @@ RSpec.describe PlaygroundsService do
   end
 
   it 'establishes connection for a single playground' do
+    stubbed_response = File.read('spec/fixtures/playground_2_data.json')
     stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/2')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent' => 'Faraday v2.7.5'
-        }
-      )
-      .to_return(status: 200, body: JSON.generate({ "data":
-                                                    {
-                                                      "id": '322474',
-                                                      "type": 'review',
-                                                      "attributes": {
-                                                        "playground_id": '2',
-                                                        "playground_name": 'Fehringer',
-                                                        "playground_address": 'Full address',
-                                                        "rating": '4.2'
-                                                      }
-                                                    } }), headers: {})
-
+    .to_return(status: 200, body: stubbed_response)
+    
     playground = PlaygroundsService.new.get_playground(2)
     expect(playground).to be_a Hash
     expect(playground).to have_key(:data)
     expect(playground[:data]).to have_key(:id)
     expect(playground[:data]).to have_key(:type)
-    expect(playground[:data][:attributes]).to have_key(:playground_id)
     expect(playground[:data][:attributes]).to have_key(:playground_name)
     expect(playground[:data][:attributes]).to have_key(:playground_address)
     expect(playground[:data][:attributes]).to have_key(:rating)
   end
 
   it 'can get reviews for specific playground' do
+    stubbed_response = File.read('spec/fixtures/playground_24_reviews.json')
     stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/24/reviews')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent' => 'Faraday v2.7.5'
-        }
-      )
-      .to_return(status: 200, body: JSON.generate({ "data": [
-                                                    {
-                                                      "id": '322458',
-                                                      "type": 'review',
-                                                      "attributes": {
-                                                        "comment": 'comment',
-                                                        "user_id": '12',
-                                                        "rating": '4.2',
-                                                        "image": 'image',
-                                                        "playground_id": '24'
-                                                      }
-                                                    }
-                                                  ] }), headers: {})
+    .to_return(status: 200, body: stubbed_response)
 
     playground = PlaygroundsService.new.get_reviews(24)
     expect(playground[:data]).to be_an Array
