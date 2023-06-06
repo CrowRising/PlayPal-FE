a705fc78713f2c9bcaaebe4c3c17b3e04e9f5f89
Thomas Hawley <thawley22@turing.edu>
2023-06-05 19:27:07 -0600

    uncomment test

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -5,39 +5,17 @@ RSpec.describe 'As a registered user' do
     before(:each) do
       @user1 = create(:user, id: 2, google_id: '123456789')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
-    #   OmniAuth.config.test_mode = true
-    #   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-    #   provider: 'google_oauth2',
-    #   uid: '123456789',
-    #   info: {
-    #     email: 'test@example.com',
-    #     name: 'Test User'
-    #   },
-    #   credentials: {
-    #     token: 'testtoken'
-    #   }
-    # )
-    # visit root_path
-    # click_link('Login With Google')
-    # stubbed_response = File.read('spec/fixtures/playgrounds_data.json')
-    # stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/123%20st/1600')
-    # .to_return(status: 200, body: stubbed_response)
+   
+      stubbed_response = File.read('spec/fixtures/playgrounds_data.json')
+      stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/123%20st/1600')
+      .to_return(status: 200, body: stubbed_response)
 
-    stubbed_response = File.read('spec/fixtures/favorites_data.json')
-    stub_request(:get, 'http://localhost:3000/api/v0/users/2/favorites')
-    .to_return(status: 200, body: stubbed_response)
-    
-    # stubbed = stub_request(:get, "http://localhost:3000/api/v0/users/2/favorites").
-    #      with(
-    #        headers: {
-    #       'Accept'=>'*/*',
-    #       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-    #       'User-Agent'=>'Faraday v2.7.5'
-    #        }).
-    #      to_return(status: 200, body: stubbed, headers: {})
+      stubbed_response = File.read('spec/fixtures/favorites_data.json')
+      stub_request(:get, 'http://localhost:3000/api/v0/users/2/favorites')
+      .to_return(status: 200, body: stubbed_response)
     end
 
-    xit "I see a search bar to search for playgrounds" do
+    it "I see a search bar to search for playgrounds" do
       visit dashboard_path
 
       expect(page).to have_field(:location)
@@ -60,7 +38,7 @@ RSpec.describe 'As a registered user' do
         expect(page).to have_content('Rating: 2.7')
       end
     end
-    
+
     it "I see a list of all my favorite playgrounds" do
       visit dashboard_path
       
@@ -68,10 +46,10 @@ RSpec.describe 'As a registered user' do
       expect(page).to have_content("My Favorite Playgrounds")
 
       within '#pg-5' do
-        expect(page).to have_content('Lehigh Park')
+        expect(page).to have_link('Lehigh Park')
       end
       within '#pg-10' do
-        expect(page).to have_content('City Park')
+        expect(page).to have_link('City Park')
       end
     end
   end
