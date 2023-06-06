fa98aee324b6bd1b7257fa1f2f41a7ec80183647
Thomas Hawley <thawley22@turing.edu>
2023-06-05 19:13:12 -0600

    Update playground id to be a string

--- spec/poros/favorite_spec.rb
+++ spec/poros/favorite_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Favorite do
       id: "2",
       type: "user_playgrounds",
       attributes: {
-        playground_id: 5,
+        playground_id: '5',
         playground_name: "Lehigh Park"
         }
     }
@@ -14,7 +14,7 @@ RSpec.describe Favorite do
     favorite = Favorite.new(fav_data)
 
     expect(favorite).to be_a Favorite
-    expect(favorite.playground_id).to eq(5)
+    expect(favorite.playground_id).to eq('5')
     expect(favorite.playground_name).to eq("Lehigh Park")
   end
 end
