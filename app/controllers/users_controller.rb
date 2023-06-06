9b8472634b18f777e6ae24ecbd4ceeeada7f2d71
Thomas Hawley <thawley22@turing.edu>
2023-06-05 19:26:15 -0600

    Update show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   def show
     if current_user
-    @facade = FavoritesFacade.new(current_user.id).playgrounds
+    @facade = FavoritesFacade.new(current_user.id)
     else
       redirect_to root_path
       flash[:error] = 'You must log in to visit this page'
