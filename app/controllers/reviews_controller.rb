722905556daf35ec7ddc74ce8222274d4413eec7
Thomas Hawley <thawley22@turing.edu>
2023-06-06 15:39:53 -0600

    refactor: Update create action with logic to determine if all fields were filled in.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,21 +9,25 @@ class ReviewsController < ApplicationController
 
   def create
     # AWS S3
-    Aws.config.update(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
-    bucket = Aws::S3::Resource.new.bucket(ENV['BUCKET_NAME'])
-    file = bucket.object(params[:image].original_filename)
-    file.upload_file(params[:image], acl: 'public-read')
-
-    review_data = {
-      user_id: params[:user_id],
-      playground_id: params[:id],
-      rating: params[:rating], 
-      image: file.public_url,
-      comment: params[:comment]
-    }
-
-    PlaygroundsService.new.add_review(review_data)
-    redirect_to "/playgrounds/#{params[:id]}", notice: 'Review was successfully created.'
+    if params_present?(params)
+      Aws.config.update(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+      bucket = Aws::S3::Resource.new.bucket(ENV['BUCKET_NAME'])
+      file = bucket.object(params[:image].original_filename)
+      file.upload_file(params[:image], acl: 'public-read')
+      
+      review_data = {
+        user_id: params[:user_id],
+        playground_id: params[:id],
+        rating: params[:rating], 
+        image: file.public_url,
+        comment: params[:comment]
+      }
+      
+      PlaygroundsService.new.add_review(review_data)
+      redirect_to "/playgrounds/#{params[:id]}", notice: 'Review was successfully created.'
+    else
+      redirect_to new_playground_review_path(params[:id]), notice: 'All fields must be filled out.'
+    end
   end
   
 
@@ -31,5 +35,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:rating, :comment, :image)
+  end
+
+  def params_present?(params)
+    params[:image].present? && params[:rating].present? && params[:comment].present?
   end
 end
