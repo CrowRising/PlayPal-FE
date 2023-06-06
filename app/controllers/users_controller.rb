cc507f5fea1a7a2a4c43d627cf589f70d78e2006
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-05 18:43:12 -0600

    Feat: controller show action
    Co-authored-by: Logan Wilson <loganwilson777@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   def show
-    # @facade = FavoritesFacade.new(current_user.id).playgrounds
+    @facade = FavoritesFacade.new(current_user.id).playgrounds
+    # require 'pry'; binding.pry
   end
 end
