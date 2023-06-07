b0f31f0b7e20857eeeac7fe5815f25fdfd886079
Crow Rising <miscbyrd@gmail.com>
2023-06-06 20:42:27 -0600

    fix: change url

--- spec/facades/playgrounds_facade_spec.rb
+++ spec/facades/playgrounds_facade_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe PlaygroundsFacade do
   before(:each) do
     stubbed_response = File.read('spec/fixtures/playgrounds_data.json')
-    stub_request(:get, 'http://localhost:3000/api/v0/playgrounds/123%20st/1600')
+    stub_request(:get, 'https://playpal-be.onrender.com/api/v0/playgrounds/123%20st/1600')
     .to_return(status: 200, body: stubbed_response)
   end
 
